92cebdd2763a3cb4307fff5bc66311658adad9dc
PolCarva <107940742+PolCarva@users.noreply.github.com>
2023-08-09 18:52:22 -0300

    built

--- client/src/constants/stable.jsx
+++ client/src/constants/stable.jsx
@@ -1,5 +1,7 @@
-const UPLOAD_FOLDER_BASE_URL = "socialmultimedia.netlify.app/uploads";
-const BASE_URL = "socialmultimedia.netlify.app";
+const UPLOAD_FOLDER_BASE_URL = "socialmultimedia.netlify.app/uploads/";
+//const UPLOAD_FOLDER_BASE_URL = "http://localhost:5000/uploads/";
+//const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "socialmultimedia.netlify.app/";
 
 const stables = {
   UPLOAD_FOLDER_BASE_URL,
