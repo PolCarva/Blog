1354ba3dbc1795d847a86528335ddf2475b20616
PolCarva <pablocarvalhogimenez@gmail.com>
2023-10-13 16:22:16 -0300

    changed baseURL to production

--- client/src/constants/stable.jsx
+++ client/src/constants/stable.jsx
@@ -1,6 +1,6 @@
 //const UPLOAD_FOLDER_BASE_URL = "socialmultimedia.netlify.app/uploads/";
-const UPLOAD_FOLDER_BASE_URL = "http://localhost:5000/uploads/";
-const BASE_URL = "http://localhost:5000/";
+const UPLOAD_FOLDER_BASE_URL = "https://socialmultimedia.onrender.com/uploads/";
+const BASE_URL = "https://socialmultimedia.onrender.com/";
 //const BASE_URL = "socialmultimedia.netlify.app/";
 
 const stables = {
