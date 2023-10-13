128bfa8cea6832c2c0a8535777c4c483cad29548
PolCarva <pablocarvalhogimenez@gmail.com>
2023-10-13 16:05:16 -0300

    trying to resolve imports

---
+++ client/src/pages/home/container/Hero.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+
+import { images } from "../../../constants";
+
+const Hero = () => {
+  return (
+    <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
+      <div className="mt-10 lg:w-1/2">
+        <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w[540px]">
+          Multimedia Social Media
+        </h1>
+        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+          Inspire and collaborate with ORT Multimedia Design students on our
+          social network. Join today to create tomorrow's design together!
+        </p>
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
+          <div className="relative">
+            <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-gray-placeholder" />
+            <input
+              type="text"
+              className="placeholder:font-bold font-semibold text-dark-soft placeholder:text-gray-placeholder rounded-lg pl-12 pr-13 w-full py-3 focus:outline-none shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] md:py-4"
+              placeholder="Search article"
+            />
+          </div>
+          <button className="w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">
+            Search
+          </button>
+        </div>
+        <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
+          <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
+            Popular Tags
+          </span>
+          <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm xl:text-base">
+            <li className="rounded-lg text-primary bg-primary bg-opacity-10 px-3 py-1.5 font-semibold">
+              Design
+            </li>
+            <li className="rounded-lg text-primary bg-primary bg-opacity-10 px-3 py-1.5 font-semibold">
+              User Experience
+            </li>
+            <li className="rounded-lg text-primary bg-primary bg-opacity-10 px-3 py-1.5 font-semibold">
+              User Interfaces
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="hidden lg:block lg:1/2">
+        <img src={images.HeroImg} alt="Users are reading articles" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
