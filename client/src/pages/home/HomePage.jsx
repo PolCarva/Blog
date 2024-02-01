7a00c233b558010a75e5d85f96e029f35cd2d7ff
PolCarva <pablocarvalhogimenez@gmail.com>
2024-01-31 21:00:08 -0300

    Add toast notification to homepage

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -3,13 +3,23 @@ import MainLayout from "../../components/MainLayout";
 import Hero from "./container/Hero";
 import Articles from "./container/Articles";
 import CTA from "./container/CTA";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  toast("Site under construction. Some features may not work!", {
+    icon: "🚧",
+    duration: 10000,
+    style: {
+      background: "#333",
+      color: "#fff",
+    },
+  });
+
   return (
     <MainLayout>
       <Hero />
       <Articles />
-      <CTA/>
+      <CTA />
     </MainLayout>
   );
 };
