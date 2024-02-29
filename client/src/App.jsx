807fe53ca7cf0eda6c9601eebac9f08ffe33bb38
PolCarva <pablocarvalhogimenez@gmail.com>
2024-02-29 11:20:31 -0300

    Add @vercel/analytics package and integrate analytics tracking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
@@ -21,29 +22,31 @@ import BlogPage from "./pages/blog/BlogPage";
 
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
-
-    <div className="App font-opensans">
-      <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<ArticleDetailPage />} />
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<Admin />} />
-          <Route path="comments" element={<Comments />} />
-          <Route path="posts/manage" element={<ManagePosts />} />
-          <Route path="posts/manage/edit/:slug" element={<EditPost />} />
-          <Route path="categories/manage" element={<Categories />} />
-          <Route path="categories/manage/edit/:slug" element={<EditCategories />} />
-          <Route path="users/manage/" element={<Users />} />
-        </Route>
-      </Routes>
-      <Toaster />
-    </div>
-    </I18nextProvider>
+    <>
+      <I18nextProvider i18n={i18n}>
+        <div className="App font-opensans">
+          <Routes>
+            <Route index path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:slug" element={<ArticleDetailPage />} />
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<Admin />} />
+              <Route path="comments" element={<Comments />} />
+              <Route path="posts/manage" element={<ManagePosts />} />
+              <Route path="posts/manage/edit/:slug" element={<EditPost />} />
+              <Route path="categories/manage" element={<Categories />} />
+              <Route path="categories/manage/edit/:slug" element={<EditCategories />} />
+              <Route path="users/manage/" element={<Users />} />
+            </Route>
+          </Routes>
+          <Toaster />
+        </div>
+      </I18nextProvider>
+      <Analytics />
+    </>
   );
 }
 
